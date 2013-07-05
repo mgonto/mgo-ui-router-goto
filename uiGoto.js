f56f9ccb17f0ec1df88843bce71d815e8e57568a
Martin Gontovnikas <martin@gonto.com.ar>
2013-07-05 16:32:30 -0300

    Removed Underscore dependency

--- uiGoto.js
+++ uiGoto.js
@@ -15,13 +15,13 @@ angular.module('mgo.ui.router.directives').directive('uiGoto', function() {
                 var params = {};
                 if ($attrs.uiGotoParams) {
                     var params = $scope.$eval($attrs.uiGotoParams);
-                    if (!_.isObject(params)) {
+                    if (!angular.isObject(params)) {
                         throw new Error("Parameters for uiGotoParams must be an object");
                     }
                 }
                 
                $scope.$apply(function() {
-                   $state.transitionTo(state, _.extend($state.params, params));
+                   $state.transitionTo(state, angular.extend({}, $state.params, params));
                }) 
             });
             
